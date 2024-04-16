2c4c56aed88c70c4f88c44c5b333e928de4e7082
francesco-mauro <francesco.mauro180@gmail.com>
2024-04-16 16:02:28 +0200

    add: answer 1 exercise bundle 01

---
+++ js-debug/bundle-01/main.js
@@ -0,0 +1,49 @@
+/*******************************************************************************
+
+    Per ognuno dei seguenti esercizi, rispondi a queste domande:
+    1- Che cosa fa questo codice?
+    2- Sono presenti errori di sintassi?
+    3- Sono presenti errori logici?
+
+    Inserisci i tuoi commenti con le risposte, sotto ciascun esercizio
+
+*******************************************************************************/
+
+
+// ESERCIZIO 1
+for (let i = 0; i > 5; i++) {
+    console.log(i);
+}
+// Non sono presenti errori di sintassi, ma il ciclo non può partire perchè i=0 non è maggiore di 5
+// Quindi per farlo funzionare correttamente (o almeno per farlo partire) possdiamo modificare la condizione i > 5 con i < 5
+
+
+// ESERCIZIO 2
+function addIfEven(num) {
+    if (num % 2 = 0) {
+        return num + 5;
+    }
+    return num;
+}
+
+
+// ESERCIZIO 3
+function loopToFive() {
+    for (let i = 0, i < 5, i++) {
+        console.log(i);
+    }
+}
+
+
+// ESERCIZIO 4 (suggerimento: ci sono 7 errori)
+function displayEvenNumbers() {
+    let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+    let evenNumbers = [];
+    for (let i = 0; i < numbers.length - 1; i++;) {
+        if (numbers % 2 = 0); {
+            evenNumbers.push(i);
+        }
+        return evenNumbers;
+    }
+}
+displayEvenNumbers(); // dovrebbe restituire [2,4,6,8]
